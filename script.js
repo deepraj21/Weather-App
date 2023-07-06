350f9dde40f8f01b685c81812adcc51eba45d7c2
Deepraj <83288891+deepraj21@users.noreply.github.com>
2023-07-06 20:14:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
+const input = document.querySelector('.search-box input');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
-search.addEventListener('click', () => {
-
+function fetchWeatherData(city) {
     const APIKey = '5cc7c2df447db16bc72940252204ca3d';
-    const city = document.querySelector('.search-box input').value;
 
-    if (city === '')
-        return;
+    if (city === '') return;
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
-
             if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
@@ -38,23 +35,18 @@ search.addEventListener('click', () => {
                 case 'Clear':
                     image.src = 'images/clear.png';
                     break;
-
                 case 'Rain':
                     image.src = 'images/rain.png';
                     break;
-
                 case 'Snow':
                     image.src = 'images/snow.png';
                     break;
-
                 case 'Clouds':
                     image.src = 'images/cloud.png';
                     break;
-
                 case 'Haze':
                     image.src = 'images/mist.png';
                     break;
-
                 default:
                     image.src = '';
             }
@@ -69,9 +61,17 @@ search.addEventListener('click', () => {
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
-
-
         });
+}
 
+search.addEventListener('click', () => {
+    const city = input.value;
+    fetchWeatherData(city);
+});
 
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const city = input.value;
+        fetchWeatherData(city);
+    }
 });
